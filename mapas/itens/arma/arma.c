48784f3dd463194f1ffb065e476be0b831327c07
Luks715 <lucastsciencia@gmail.com>
2026-07-10 17:33:36 -0300

    adicionando logica de construção para itens

---
+++ mapas/itens/arma/arma.c
@@ -0,0 +1,35 @@
+#include "../../../include/entity.h"
+#include "arma.h"
+
+void arma_collision(Entity *self, Entity *others){}
+
+typedef enum {
+    IDLE,
+} arma_state;
+
+static State idle = {
+    .id                    = IDLE,
+    .allowed_transitions  = {},
+    .count                = 0,
+    .animation            = &anim_idle,
+    .evaluate_entry       = NULL,
+    .evaluate_exit        = NULL,
+    .decide_input         = NULL,
+};
+
+const Entity *arma_create(int x, int y, int h_dir, int v_dir){
+    Entity *e = entity_alloc();
+    e->position     = (Coordinates){y, x};
+    e->velocity     = (Coordinates){0, 0};
+    e->type         = ENTITY_ITEM;
+    e->orientation  = (Orientation){ h_dir, v_dir};
+    e->hitbox       = (Hitbox){
+        .type       = HITBOX_RECTANGLE,
+        .data       = { .rectangle={ 16, 32 } },
+        .get_cells  = get_rectangle_cells,
+    };
+    e->sm.current_state = &idle;
+    e->sm.transition    = NULL;
+    e->on_collision     = arma_collision;
+    return e;
+};

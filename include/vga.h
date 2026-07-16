38a9a799f2da8b43d05640877c18934c842bae17
Luks715 <lucastsciencia@gmail.com>
2026-07-16 08:40:21 -0300

    declaração das funções display para chamar na main

--- include/vga.h
+++ include/vga.h
@@ -6,3 +6,5 @@ extern volatile uint16_t (*tela)[LWIDTH];
 void clear_screen(uint16_t color);
 int vga_init(void);
 void swap_buffers(void);
+void display_score(int score);
+void display_live(int live);
